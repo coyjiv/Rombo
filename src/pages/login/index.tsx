29ec4cdf9eb777917c1fd4a9c686239c73cd252f
1Maxuta1 <maksklas_1@ukr.net>
2023-08-03 01:43:39 +0300

    g

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,8 @@
-type Props = {}
+import { Formik, Form, Field, ErrorMessage } from "formik";
+type Props = {};
 
 const LoginPage = (props: Props) => {
-  return (
-    <div>index</div>
-  )
-}
+  return <div>index</div>;
+};
 
-export default LoginPage
+export default LoginPage;
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,9 +1,28 @@
-type Props = {}
+import { Formik, Form, Field, ErrorMessage } from "formik";
+type Props = {};
 
 const RegistrationPage = (props: Props) => {
   return (
-    <div>register</div>
-  )
-}
-
-export default RegistrationPage
+    <div>
+      <Formik
+        initialValues={{ firstName: "", lastName: "", email: "", password: "" }}
+        onSubmit={(values) => {
+          // Функция, которая будет вызвана при отправке формы
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div>
+              <label htmlFor="fieldName">Поле:</label>
+              <Field type="text" id="fieldName" name="fieldName" />
+              <ErrorMessage name="fieldName" component="div" />
+            </div>
+            <button type="submit">Отправить</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+export default RegistrationPage;
