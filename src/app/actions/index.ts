508a374f1527abc9b949741247fa99a04bc2e7cd
Maksym Turlo <maksklas_1@ukr.net>
2023-09-07 22:08:37 +0300

    update 2.0

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,25 +1,25 @@
-import { LoginValues, User } from "@/types";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+// import { LoginValues, User } from "@/types";
+// import { createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchUserById = createAsyncThunk<User>(
-  "user/fetchById",
-  async () => {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1"
-    );
-    return response.json() as any;
-  }
-);
-export const login = createAsyncThunk<Promise<any>>(
-  "user/login",
-  async (formData : LoginValues) => {
-    const response = await fetch("/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(formData),
-    });
-    return response.json() as any;
-  }
-);
+// export const fetchUserById = createAsyncThunk<User>(
+//   "user/fetchById",
+//   async () => {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/todos/1"
+//     );
+//     return response.json() as any;
+//   }
+// );
+// export const login = createAsyncThunk<Promise<any>>(
+//   "user/login",
+//   async (formData : LoginValues) => {
+//     const response = await fetch("/api/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+//     return response.json() as any;
+//   }
+// );
